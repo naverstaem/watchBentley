fea1ccd405ff75c8022e823e853f41a42714574f
fanian <rfanian@gmail.com>
2014-09-30 15:07:13 +0400

    add other block

--- js/common.js
+++ js/common.js
@@ -3,15 +3,15 @@ head.ready(function() {
 	// $(document).on("click", function(){
 	// 	$(".js-popup").hide();
 	// });
-//    $(window).scroll(function () {
-//        if ( $(this).scrollTop() > 200 && !$('.scrollMenu').hasClass('open') ) {
-//            $('.scrollMenu').addClass('open');
-//            $('.scrollMenu').slideDown();
-//        } else if ( $(this).scrollTop() <= 200 ) {
-//            $('.scrollMenu').removeClass('open');
-//            $('.scrollMenu').slideUp();
-//        }
-//    });
+    $(window).scroll(function () {
+        if ( $(this).scrollTop() > 200 && !$('.scrollMenu').hasClass('open') ) {
+            $('.scrollMenu').addClass('open');
+            $('.scrollMenu').slideDown();
+        } else if ( $(this).scrollTop() <= 200 ) {
+            $('.scrollMenu').removeClass('open');
+            $('.scrollMenu').slideUp();
+        }
+    });
 //    jQuery(document).ready(function($) {
 //        var nav = $('.menu');
 //
@@ -43,8 +43,18 @@ head.ready(function() {
             overlay: {
                 locked: false
             }
+        },
+        beforeShow: function(){
+            $(".fancybox-skin").css("backgroundColor","transparent");
+            $(".fancybox-skin").css("boxShadow","none");
         }
     });
+
+
+     $('.slick').slick({
+         slidesToShow: 3,
+         slidesToScroll: 1
+     });
 
 //
 //    $('.fancybox').fancybox({
